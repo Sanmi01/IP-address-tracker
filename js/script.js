77ea6189dd046691e1947d9720e2836cf3b277d1
Sanmi01 <supersanmi01@gmail.com>
2021-01-05 00:40:24 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-// var ip = "8.8.8.8";
-// var api_key = "at_pfRaPduq4NcjNbWOyhqrHXKUURmbv";
-//     $(function () {
-//        $.ajax({
-//            url: "https://geo.ipify.org/api/v1",
-//            data: {apiKey: api_key, ipAddress: ip},
-//            success: function(data) {
-//                $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
-//            }
-//        });
-//        console.log(data);
-//     });
-// getData = () => {
-//     url: "https://geo.ipify.org/api/v1",
-//      data: {apiKey: api_key, ipAddress: ip},
+// // var ip = "8.8.8.8";
+// // var api_key = "at_pfRaPduq4NcjNbWOyhqrHXKUURmbv";
+// //     $(function () {
+// //        $.ajax({
+// //            url: "https://geo.ipify.org/api/v1",
+// //            data: {apiKey: api_key, ipAddress: ip},
+// //            success: function(data) {
+// //                $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
+// //            }
+// //        });
+// //        console.log(data);
+// //     });
+// // getData = () => {
+// //     url: "https://geo.ipify.org/api/v1",
+// //      data: {apiKey: api_key, ipAddress: ip},
 
-// }
+// // }
 
 
-var map = L.map('map').setView([0, 0], 1);
+// var map = L.map('map').setView([0, 0], 1);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
 
-L.marker([0, 0]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+// L.marker([0, 0]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
